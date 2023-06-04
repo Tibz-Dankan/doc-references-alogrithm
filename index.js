d6527506c7301db2d99491e7406de4d8d40adbb0
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-06-04 14:27:30 +0300

    feat: add fullstops when encounter semicolon

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("App initialized");
+const readline = require("readline");
+
+function addFullStop(string, maxLength) {
+  let count = 0;
+  let result = "";
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === ":") {
+      result += ":";
+      count++;
+      while (count < maxLength) {
+        result += ".";
+        count++;
+      }
+    } else {
+      result += string[i];
+      count++;
+    }
+
+    if (count >= maxLength) {
+      break;
+    }
+  }
+
+  return result;
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Enter a string: ", (inputString) => {
+  rl.question("Enter the maximum length: ", (maxLength) => {
+    const modifiedString = addFullStop(inputString, parseInt(maxLength));
+    console.log("Modified string:", modifiedString);
+    rl.close();
+  });
+});
